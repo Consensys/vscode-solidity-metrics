f1afbd7070c3ca8dd0c8ee9c277edd37543e1532
tintinweb <tintinweb@oststrom.com>
2020-04-03 16:14:50 +0200

    fix git repo info

--- src/features/metrics.js
+++ src/features/metrics.js
@@ -226,7 +226,7 @@ This section lists files that are in scope for the metrics report.
 - **File Limit:** \`${this.inputFileGlobLimit}\`
     - **Exclude File list Limit:** \`${this.excludeFileGlobLimit}\`
 
-- **Repository:** Workspace repository (\`${this.repoInfo.commit}@${this.repoInfo.branch}\`)
+- **Repository:** Workspace repository (\`${this.repoInfo.branch}\`@\`${this.repoInfo.commit}\`)
 
 ### <span id=t-source-Units-in-Scope>Source Units in Scope</span>
 
